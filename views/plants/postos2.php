deab052b3aea8439be4b53fc7248f9beafad108a
ViniciusPereira99 <79757554+ViniciusPereira99@users.noreply.github.com>
2021-03-10 02:35:04 -0400

    postos se movimentando

--- views/plants/postos2.php
+++ views/plants/postos2.php
@@ -1,239 +1,109 @@
 <?php
 
 use app\models\Plants;
-use app\models\PlantsPostos;
 use yii\helpers\Html;
-use yii\widgets\DetailView;
 use app\controllers\PlantsController;
-
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Plants */
 
-$this->title = $model->Nome_da_Planta;
-$this->params['breadcrumbs'][] = ['label' => 'Plants', 'url' => ['index']];
+$this->title = 'Virtualização';
 $this->params['breadcrumbs'][] = $this->title;
-\yii\web\YiiAsset::register($this);
 
-function atualizaBd($id_postos,$id_principal)
-{
-    $planta = Plants::findOne($id_principal);
-    $plantaPostos = PlantsPostos::findOne($id_postos);
-    $plantaPostos->Status_M120 = $planta->Status_M120;
-    $plantaPostos->Status_M121 = $planta->Status_M121;
-    $plantaPostos->Status_M122 = $planta->Status_M122;
-    $plantaPostos->Status_M123 = $planta->Status_M123;
-    $plantaPostos->Status_M124 = $planta->Status_M124;
-    $plantaPostos->Status_M125 = $planta->Status_M125;
-    $plantaPostos->Status_M126 = $planta->Status_M126;
-    $plantaPostos->Status_M127 = $planta->Status_M127;
-    $plantaPostos->Status_M128 = $planta->Status_M128;
-    $plantaPostos->Status_M129 = $planta->Status_M129;
-    $plantaPostos->Status_M130 = $planta->Status_M130;
-    $plantaPostos->Status_M131 = $planta->Status_M131;
-    $plantaPostos->Status_M132 = $planta->Status_M132;
-    $plantaPostos->Status_M133 = $planta->Status_M133;
-    $plantaPostos->Status_M134 = $planta->Status_M134;
-    $plantaPostos->Status_M135 = $planta->Status_M135;
-    $plantaPostos->Status_M136 = $planta->Status_M136;
-    $plantaPostos->Status_M137 = $planta->Status_M137;
-    $plantaPostos->Status_M138 = $planta->Status_M138;
-    $plantaPostos->Status_M139 = $planta->Status_M139;
-    $plantaPostos->save();
-}
-/*
-function novo_posto($idposto)
-{
-    $novoposto = new PlantsPostos;
-    $a = Plants::findOne($idposto);
-    $novoposto->Nome_da_Planta = "teste linha 3";
-    $novoposto->save();
-    print_r($novoposto);
-}
-*/
-function atualizar_bd_comparacao()
-{
-    $cont_dif=0;
-    $plantaPostos_status_prev = PlantsPostos::findOne(1);
-    $plantaPostos_status_atual = PlantsPostos::findOne(2);
-    if($plantaPostos_status_prev->Status_M120!=$plantaPostos_status_atual->Status_M120){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M121!=$plantaPostos_status_atual->Status_M121){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M122!=$plantaPostos_status_atual->Status_M122){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M123!=$plantaPostos_status_atual->Status_M123){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M124!=$plantaPostos_status_atual->Status_M124){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M125!=$plantaPostos_status_atual->Status_M125){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M126!=$plantaPostos_status_atual->Status_M126){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M127!=$plantaPostos_status_atual->Status_M127){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M128!=$plantaPostos_status_atual->Status_M128){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M129!=$plantaPostos_status_atual->Status_M129){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M130!=$plantaPostos_status_atual->Status_M130){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M131!=$plantaPostos_status_atual->Status_M131){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M132!=$plantaPostos_status_atual->Status_M132){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M133!=$plantaPostos_status_atual->Status_M133){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M134!=$plantaPostos_status_atual->Status_M134){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M135!=$plantaPostos_status_atual->Status_M135){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M136!=$plantaPostos_status_atual->Status_M136){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M137!=$plantaPostos_status_atual->Status_M137){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M138!=$plantaPostos_status_atual->Status_M138){$cont_dif++;}
-    if($plantaPostos_status_prev->Status_M139!=$plantaPostos_status_atual->Status_M139){$cont_dif++;}
+$planta = Plants::findOne($model);
+$StatusES = array();
+$startbt;
 
-    if($cont_dif > 0)
-    {
-        $novoposto = new PlantsPostos;
-        $novoposto->Nome_da_Planta = "Posto1";
-        $novoposto->Status_M120 = $plantaPostos_status_prev->Status_M120;
-        $novoposto->Status_M121 = $plantaPostos_status_prev->Status_M121;
-        $novoposto->Status_M122 = $plantaPostos_status_prev->Status_M122;
-        $novoposto->Status_M123 = $plantaPostos_status_prev->Status_M123;
-        $novoposto->Status_M124 = $plantaPostos_status_prev->Status_M124;
-        $novoposto->Status_M125 = $plantaPostos_status_prev->Status_M125;
-        $novoposto->Status_M126 = $plantaPostos_status_prev->Status_M126;
-        $novoposto->Status_M127 = $plantaPostos_status_prev->Status_M127;
-        $novoposto->Status_M128 = $plantaPostos_status_prev->Status_M128;
-        $novoposto->Status_M129 = $plantaPostos_status_prev->Status_M129;
-        $novoposto->Status_M130 = $plantaPostos_status_prev->Status_M130;
-        $novoposto->Status_M131 = $plantaPostos_status_prev->Status_M131;
-        $novoposto->Status_M132 = $plantaPostos_status_prev->Status_M132;
-        $novoposto->Status_M133 = $plantaPostos_status_prev->Status_M133;
-        $novoposto->Status_M134 = $plantaPostos_status_prev->Status_M134;
-        $novoposto->Status_M135 = $plantaPostos_status_prev->Status_M135;
-        $novoposto->Status_M136 = $plantaPostos_status_prev->Status_M136;
-        $novoposto->Status_M137 = $plantaPostos_status_prev->Status_M137;
-        $novoposto->Status_M138 = $plantaPostos_status_prev->Status_M138;
-        $novoposto->Status_M139 = $plantaPostos_status_prev->Status_M139;
-        $novoposto->save();
+if($planta->Entrada_0){$StatusES[]="Status_M100";}
+if($planta->Entrada_1){$StatusES[]="Status_M101";}
+if($planta->Entrada_2){$StatusES[]="Status_M102";}
+if($planta->Entrada_3){$StatusES[]="Status_M103";}
+if($planta->Entrada_4){$StatusES[]="Status_M104";}
+if($planta->Entrada_5){$StatusES[]="Status_M105";}
+if($planta->Entrada_6){$StatusES[]="Status_M106";}
+if($planta->Entrada_7){$StatusES[]="Status_M107";}
+if($planta->Entrada_8){$StatusES[]="Status_M108";}
+if($planta->Entrada_9){$StatusES[]="Status_M109";}
+if($planta->Entrada_10){$StatusES[]="Status_M110";}
+if($planta->Entrada_11){$StatusES[]="Status_M111";}
+if($planta->Entrada_12){$StatusES[]="Status_M112";}
+if($planta->Entrada_13){$StatusES[]="Status_M113";}
+if($planta->Entrada_14){$StatusES[]="Status_M114";}
+if($planta->Entrada_15){$StatusES[]="Status_M115";}
+if($planta->Entrada_16){$StatusES[]="Status_M116";}
+if($planta->Entrada_17){$StatusES[]="Status_M117";}
+if($planta->Entrada_18){$StatusES[]="Status_M118";}
+if($planta->Entrada_19){$StatusES[]="Status_M119";}
+if($planta->Saída_0){$StatusES[]="Status_M120";}
+if($planta->Saída_1){$StatusES[]="Status_M121";}
+if($planta->Saída_2){$StatusES[]="Status_M122";}
+if($planta->Saída_3){$StatusES[]="Status_M123";}
+if($planta->Saída_4){$StatusES[]="Status_M124";}
+if($planta->Saída_5){$StatusES[]="Status_M125";}
+if($planta->Saída_6){$StatusES[]="Status_M126";}
+if($planta->Saída_7){$StatusES[]="Status_M127";}
+if($planta->Saída_8){$StatusES[]="Status_M128";}
+if($planta->Saída_9){$StatusES[]="Status_M129";}
+if($planta->Saída_10){$StatusES[]="Status_M130";}
+if($planta->Saída_11){$StatusES[]="Status_M131";}
+if($planta->Saída_12){$StatusES[]="Status_M132";}
+if($planta->Saída_13){$StatusES[]="Status_M133";}
+if($planta->Saída_14){$StatusES[]="Status_M134";}
+if($planta->Saída_15){$StatusES[]="Status_M135";}
+if($planta->Saída_16){$StatusES[]="Status_M136";}
+if($planta->Saída_17){$StatusES[]="Status_M137";}
+if($planta->Saída_18){$StatusES[]="Status_M138";}
+if($planta->Saída_19){$StatusES[]="Status_M139";}
 
-        $plantaPostos_status_atual->Status_M120 = $plantaPostos_status_prev->Status_M120;
-        $plantaPostos_status_atual->Status_M121 = $plantaPostos_status_prev->Status_M121;
-        $plantaPostos_status_atual->Status_M122 = $plantaPostos_status_prev->Status_M122;
-        $plantaPostos_status_atual->Status_M123 = $plantaPostos_status_prev->Status_M123;
-        $plantaPostos_status_atual->Status_M124 = $plantaPostos_status_prev->Status_M124;
-        $plantaPostos_status_atual->Status_M125 = $plantaPostos_status_prev->Status_M125;
-        $plantaPostos_status_atual->Status_M126 = $plantaPostos_status_prev->Status_M126;
-        $plantaPostos_status_atual->Status_M127 = $plantaPostos_status_prev->Status_M127;
-        $plantaPostos_status_atual->Status_M128 = $plantaPostos_status_prev->Status_M128;
-        $plantaPostos_status_atual->Status_M129 = $plantaPostos_status_prev->Status_M129;
-        $plantaPostos_status_atual->Status_M130 = $plantaPostos_status_prev->Status_M130;
-        $plantaPostos_status_atual->Status_M131 = $plantaPostos_status_prev->Status_M131;
-        $plantaPostos_status_atual->Status_M132 = $plantaPostos_status_prev->Status_M132;
-        $plantaPostos_status_atual->Status_M133 = $plantaPostos_status_prev->Status_M133;
-        $plantaPostos_status_atual->Status_M134 = $plantaPostos_status_prev->Status_M134;
-        $plantaPostos_status_atual->Status_M135 = $plantaPostos_status_prev->Status_M135;
-        $plantaPostos_status_atual->Status_M136 = $plantaPostos_status_prev->Status_M136;
-        $plantaPostos_status_atual->Status_M137 = $plantaPostos_status_prev->Status_M137;
-        $plantaPostos_status_atual->Status_M138 = $plantaPostos_status_prev->Status_M138;
-        $plantaPostos_status_atual->Status_M139 = $plantaPostos_status_prev->Status_M139;
-        $plantaPostos_status_atual->save();
-
-        print_r("teste diferente");
-    }
-
-}
-
-//atualizaBd(2,$model->Id);
-$plantaPostos1 = PlantsPostos::findOne(4);
 ?>
 
-<div class="plants-view">
-    
-    <div id="refresh">
-        <div id="time">
-           
-           <?php
-           //atualizaBd(1,$model->Id);
-           //atualizar_bd_comparacao();
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        
+        <canvas id="teladireita" width="1000" height="600" style="border:1px solid #000000;"></canvas>
 
-            ?>
+        <script type="text/javascript">
+        var canvas = document.getElementById("teladireita");
+        var ctx1 = canvas.getContext("2d");
+        var pos1 = {x:0,y:0};
+        var pos2 = {x:100,y:0};
+        var pos3 = {x:200,y:0};
+        var pos4 = {x:300,y:0};
+        var pos5 = {x:400,y:0};
+        ctx1.fillStyle="#00FA9A";  //verde
+        ctx1.fillRect(pos1.x,pos1.y,50,50);
+        ctx1.fillRect(pos2.x,pos2.y,50,50);
+        ctx1.fillRect(pos3.x,pos3.y,50,50);
+        ctx1.fillRect(pos4.x,pos4.y,50,50);
+        ctx1.fillRect(pos5.x,pos5.y,50,50);
+        var int = 1;
+        function next(i) {
+            var canvas = document.getElementById('teladireita');
+            var ctx = canvas.getContext('2d');
+            if(i==1){pos = pos1;i=2;pos_1 = pos5;}
+            else if(i==2){pos = pos2;i=3;pos_1 = pos1;}
+            else if(i==3){pos = pos3;i=4;pos_1 = pos2;}
+            else if(i==4){pos = pos4;i=5;pos_1 = pos3;}
+            else{pos = pos5;i=1;pos_1 = pos4;}
+            int = i;
+            ctx.fillStyle = "#00FA9A";
+            ctx.fillRect(pos_1.x,pos_1.y,50,50);           
+            ctx.beginPath();
+            ctx.arc(pos.x+25, pos.y+25, 10, 0, Math.PI * 2, true); // Outer circle
+            ctx.fillStyle = 'rgb(0, 0, 0)';
+            ctx.fill();
+        }
+        setInterval("my_function();",1000); 
+        function my_function(){
+            next(int);
+            $('#refresh').load(location.href + ' #time');
+        }
+        </script>
 
+    </body>
 
-            <?= DetailView::widget([
-                'model' => $plantaPostos1,
-                'attributes' => [
-                'Id',
-                'Nome_da_Planta',
-                'Entrada_0',
-                'Status_M100',
-                'Entrada_1',
-                'Status_M101',
-                'Entrada_2',
-                'Status_M102',
-                'Entrada_3',
-                'Status_M103',
-                'Entrada_4',
-                'Status_M104',
-                'Entrada_5',
-                'Status_M105',
-                'Entrada_6',
-                'Status_M106',
-                'Entrada_7',
-                'Status_M107',
-                'Entrada_8',
-                'Status_M108',
-                'Entrada_9',
-                'Status_M109',
-                'Entrada_10',
-                'Status_M110',
-                'Entrada_11',
-                'Status_M111',
-                'Entrada_12',
-                'Status_M112',
-                'Entrada_13',
-                'Status_M113',
-                'Entrada_14',
-                'Status_M114',
-                'Entrada_15',
-                'Status_M115',
-                'Entrada_16',
-                'Status_M116',
-                'Entrada_17',
-                'Status_M117',
-                'Entrada_18',
-                'Status_M118',
-                'Entrada_19',
-                'Status_M119',
-                'Saída_0',
-                'Status_M120',
-                'Saída_1',
-                'Status_M121',
-                'Saída_2',
-                'Status_M122',
-                'Saída_3',
-                'Status_M123',
-                'Saída_4',
-                'Status_M124',
-                'Saída_5',
-                'Status_M125',
-                'Saída_6',
-                'Status_M126',
-                'Saída_7',
-                'Status_M127',
-                'Saída_8',
-                'Status_M128',
-                'Saída_9',
-                'Status_M129',
-                'Saída_10',
-                'Status_M130',
-                'Saída_11',
-                'Status_M131',
-                'Saída_12',
-                'Status_M132',
-                'Saída_13',
-                'Status_M133',
-                'Saída_14',
-                'Status_M134',
-                'Saída_15',
-                'Status_M135',
-                'Saída_16',
-                'Status_M136',
-                'Saída_17',
-                'Status_M137',
-                'Saída_18',
-                'Status_M138',
-                'Saída_19',
-                'Status_M139',
-                ],
-            ]) ?>
-        </div>
-    </div>     
+</html>
 
-</div>
